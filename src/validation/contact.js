50ebc7ac4bea3e343ec6bf37e9d71717b0034a7a
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-06-23 15:36:38 +0300

    push4vd

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -3,8 +3,8 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string()
-        .length(12)
-        .pattern(/^\+?\d{11}$/)
+        .length(13)
+        .pattern(/^\+?\d{12}$/)
         .required(),
     email: Joi.string().email().allow(null),
     isFavourite: Joi.boolean(),
